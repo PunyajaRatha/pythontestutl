f058b5fb00421f8764bec8f4596dae485117f0ba
PUNYAJA RATH <58202081+PunyajaRatha@users.noreply.github.com>
2020-04-18 12:40:22 -0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,44 @@
+"""textutils URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views#enter from views path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',views.index,name='index'),#its reload the index path
+#     path('about',views.about,name='about')#its reload the about path
+    
+# ]
+
+
+# Code for video 7
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index2, name='index2'),
+    # path('', views.index, name='index'),
+     
+    path('analyze', views.analyze, name='analyze'),
+    path('ex1', views.ex1, name='ex1'),
+
+
+    # path('removepunc', views.removepunc, name='rempun'),
+    # path('capitalizefirst', views.capfirst, name='capfirst'),
+    #  path('newlineremove', views.newlineremove, name='newlineremove'),
+    #  path('spaceremove', views.spaceremove, name='spaceremove'),
+    #  path('charcount', views.charcount, name='charcount'),
+
+]
